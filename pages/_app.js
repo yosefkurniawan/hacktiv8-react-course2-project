58d65d533acffbc1627a0e8b4b361a9b9648fc81
yosefkurniawan <jojo@icube.us>
2020-03-16 10:28:50 +0700

    Remove Container as it is deprecated

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import "../styles.css";
-import App, { Container } from 'next/app';
+import App from 'next/app';
 import { Provider } from 'react-redux';
 import withRedux from 'next-redux-wrapper';
 import { initStore } from '../redux/store';
@@ -20,11 +20,9 @@ export default withRedux(initStore, {debug: true}) (
         render() {
             const {Component, pageProps, store} = this.props;
             return (
-                <Container>
-                    <Provider store={store}>
-                        <Component {...pageProps} />
-                    </Provider>
-                </Container>
+                <Provider store={store}>
+                    <Component {...pageProps} />
+                </Provider>
             )
         }
     }
