1bd0c3b118e8774ec723ebe335df207150706a49
yosefkurniawan <jojo@icube.us>
2020-03-16 10:30:37 +0700

    apply styling on addtocart button

--- components/buttons/addToCart.js
+++ components/buttons/addToCart.js
@@ -1,9 +1,7 @@
 import Button from "./default";
-
+import styles from "./addToCart.module.css";
 const AddToCart = () => {
-    return (
-        <Button className="add-to-cart" title="Add to Cart" />
-    )
+    return <Button className={`add-to-cart ${styles.addToCart}`} title="Add to Cart" />;
 };
 
 export default AddToCart;
