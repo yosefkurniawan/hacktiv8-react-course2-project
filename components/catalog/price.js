c5a0f193d373cb120783c342a98f7b7ae7a6c763
yosefkurniawan <jojo@icube.us>
2020-03-16 15:40:53 +0700

    Change the way formatting price to solve different content between SSR and client side

--- components/catalog/price.js
+++ components/catalog/price.js
@@ -1,10 +1,10 @@
 function Price({price}) {
-    const formatter = new Intl.NumberFormat("id-ID", {
-        style: "currency",
-        currency: "IDR",
-        minimumFractionDigits: 2
-    });
-    return <div className="price">{formatter.format(price)}</div>;
+    let _price = price
+            .toFixed(2) // always two decimal digits
+            .replace(".", ",") // replace decimal point character with ,
+            .replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.");
+    _price = "Rp " + _price;
+    return <span className="price">{_price}</span>;
 }
 
 export default Price
