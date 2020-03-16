83cc219a372744e86f1c354fd80d26fd96ed8339
yosefkurniawan <jojo@icube.us>
2020-03-16 10:28:30 +0700

    Apply get single product data in PDP

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -4,42 +4,28 @@ import AddToCart from '../../components/buttons/addToCart';
 import AddToWishlist from '../../components/buttons/addToWishlist';
 import Layout from "../../components/layout/1column";
 import { useRouter } from "next/router";
+import { connect } from 'react-redux';
 
-function ProductContent() {
+function ProductContent({catalog}) {
     const router = useRouter();
     const id = router.query.id;
-    // const products = [
-    //     {
-    //         id: 1,
-    //         name: "Mi Note 10",
-    //         image:
-    //             "https://i01.appmifile.com/webfile/globalimg/products/pc/mi-note-10/specs-img01.png",
-    //         desc:
-    //             "Tempor excepteur aute elit anim adipisicing culpa est. Lorem culpa laboris voluptate elit aliqua aliquip aute. Sint proident culpa aliquip do. Aliqua exercitation do quis culpa aute sit occaecat veniam. Commodo proident tempor voluptate cupidatat ut. Dolor minim amet consequat irure in voluptate amet exercitation elit. Laborum esse adipisicing dolor dolor consectetur est.",
-    //         price: "Rp 6.199.000"
-    //     },
-    //     {
-    //         id: 2,
-    //         name: "Redmi Note 8 Pro",
-    //         image:
-    //             "https://i01.appmifile.com/webfile/globalimg/products/pc/redmi-note-8-pro/g7_title_phone_p.png",
-    //         desc:
-    //             "Tempor excepteur aute elit anim adipisicing culpa est. Lorem culpa laboris voluptate elit aliqua aliquip aute. Sint proident culpa aliquip do. Aliqua exercitation do quis culpa aute sit occaecat veniam. Commodo proident tempor voluptate cupidatat ut. Dolor minim amet consequat irure in voluptate amet exercitation elit. Laborum esse adipisicing dolor dolor consectetur est.",
-    //         price: "Rp 4.399.000"
-    //     }
-    // ];
-    // const productIndex = products.findIndex(product => product.id === parseInt(id));
-    // const product = products[productIndex];
-    const product = {
-        id: 1,
-        name: "Mi Note 10",
-        image:
-            "https://i01.appmifile.com/webfile/globalimg/products/pc/mi-note-10/specs-img01.png",
-        desc:
-            "Tempor excepteur aute elit anim adipisicing culpa est. Lorem culpa laboris voluptate elit aliqua aliquip aute. Sint proident culpa aliquip do. Aliqua exercitation do quis culpa aute sit occaecat veniam. Commodo proident tempor voluptate cupidatat ut. Dolor minim amet consequat irure in voluptate amet exercitation elit. Laborum esse adipisicing dolor dolor consectetur est.",
-        price: "Rp 6.199.000"
-    };
-    console.log(product);
+    const product = catalog.filter((product) => product.id === parseInt(id))[0];
+
+    // console.log('product');
+    // console.log(catalog);
+    // console.log(id);
+    // console.log(product);
+
+    // product = {
+    //     id: 1,
+    //     name: "Mi Note 10",
+    //     image:
+    //         "https://i01.appmifile.com/webfile/globalimg/products/pc/mi-note-10/specs-img01.png",
+    //     desc:
+    //         "Tempor excepteur aute elit anim adipisicing culpa est. Lorem culpa laboris voluptate elit aliqua aliquip aute. Sint proident culpa aliquip do. Aliqua exercitation do quis culpa aute sit occaecat veniam. Commodo proident tempor voluptate cupidatat ut. Dolor minim amet consequat irure in voluptate amet exercitation elit. Laborum esse adipisicing dolor dolor consectetur est.",
+    //     price: "Rp 6.199.000"
+    // };
+    
     return (
         <div className="page-product">
             <div className="product-top">
@@ -63,4 +49,14 @@ function ProductContent() {
     );
 }
 
-export default Layout(ProductContent);
+const mapStateToProps = (state) => ({
+    catalog: state.catalog
+})
+
+// export default ProductContent;
+export default Layout(
+    connect(
+        mapStateToProps,
+        null
+    )(ProductContent)
+);
